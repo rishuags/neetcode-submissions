5e518b2a69a2b08bbd56c4fbff31328a8d315e7f
rishuags <107926886+rishuags@users.noreply.github.com>
2026-08-08 17:44:22 +0100

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int maxArea(int[] heights) {
+
+        // find max height (min of two pillars) * width 
+        // option 1: brute force check every combo (horrible O(n^2))
+        // option 2: sort + store height / index in map  and then check
+
+        // option 3: use two pointers at opposite ends for max width & shift smaller one 
+
+        // discarding more problematic pointer / while also maintaining worst and best case as O(n)
+
+        int left =0;
+        int right = heights.length-1;
+        int answer=0;
+        int area;
+
+        while (left<right){
+            area=Math.min(heights[left], heights[right]) * (right-left);
+            answer = Math.max(answer, area);
+            if(heights[right] >= heights[left]){
+                left++;
+            }else{
+                right--;
+            }
+
+
+        }
+
+
+
+        
+        return answer;
+
+
+        
+    }
+}
