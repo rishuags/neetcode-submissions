9d1a5fd5ade4bab023fa1a021251c888b647da66
rishuags <107926886+rishuags@users.noreply.github.com>
2026-08-29 15:06:20 +0100

    Add: lowest-common-ancestor-in-binary-search-tree - submission-0

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-0.java
@@ -0,0 +1,69 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        // traverse from root (bfs or dfs) until you find p and q 
+
+        // once both are found, work your way back? 
+
+        // use bst properties to find it? 
+
+        // p is less than root so left subtree, 8 is more than root so right subtree
+        // root.left and root.right
+        // p found and q found
+
+        ///////
+
+        // if p = 4, and q = 7 
+        // common ancestor is 5
+
+        // start at root, p < root, left subtree, root.left, 
+        // q > root, right subtree, root.right 
+
+        // currRight pointer is at 3, currLeft pointer is at 8 
+
+        // p > currLeft, currLeft = currLeft.right (else, currLeft=currLeft.left)
+
+        // q < currRight, currRight = currRight.left (else currRight=currRight.right)
+
+
+        TreeNode curr = root;
+
+        //if p < root and q < root left subtree, curr=curr.left
+        //if p > root and q > root right subtree, curr=curr.right
+        //if p < root and q > root, ancestor = root  
+        // if p or q == root, root
+
+        while(curr!=null){
+            if(p.val<curr.val && q.val<curr.val){
+                curr=curr.left;
+            } else if(p.val>curr.val && q.val>curr.val){
+                curr=curr.right;
+            } else {
+                return curr;
+            }
+        }
+
+
+
+
+        return null;
+
+        
+    }
+}
